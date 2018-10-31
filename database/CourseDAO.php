bbe551ead4a5322d6b902c29a00e4db18851ca2b
bodog <bodog@192.168.1.105>
2018-10-31 17:27:11 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- database/CourseDAO.php
+++ database/CourseDAO.php
@@ -6,6 +6,9 @@
  * and open the template in the editor.
  */
 
+namespace database;
+use model\Course;
+
 /**
  * Description of CourseController
  *
@@ -13,9 +16,51 @@
  */
 class CourseDAO extends BasicDAO {
     
-    public function create(){
+    public function create(Course $course){
         $stmt = $this->$mysqli->prepare("INSERT INTO course (ID, Name, PostCode, Place, Costs, Start, End, Link, InstituteID, DepartmentID, AreaID, CourseTypeID)"
-                . "VALUES (NULL, ?, ?, ?, ?, ?, ?, ?,"
+                . "VALUES (?, ?, ?, ?, ?, ?, ?, ?,"
                 . "?, ?, ?, ?)");
+        
+        $stmt->bind_param("isisiiisiiii", NULL, $course->getName(), $course->getPostCode(), $course->getPlace(), $course->getCosts(), $course->getStart(), 
+                $course->getEnd(), $course->getLink(), $course->getInstituteId(), 
+                $course->getDepartmentId(), $course->getAreaId(), $course->getCourseTypeId());
+        
+        $stmt->execute();
+        
+        return $this->read($this->mysqli->lastInsertId());
+
+    }
+    
+    public function read($courseId){
+        $stmt = $this->mysqli->prepare("Select * from course where id = ?");
+        $stmt->bind_param("i", $courseId);
+        
+        if($stmt->execute()){
+            return $stmt->fetch_all($mysqli::fetch_object)[0];
+        }
+        
+        return null;
+    }
+    
+    public function update(Course $course){
+        $stmt = $this->mysqli->prepare("Update course set Name = ?, PostCode = ?, Place = ?, Costs = ?, Start = ?, End = ?, Link = ?, InstituteID = ?, DepartmentID = ?,"
+                . "AreaID = ?, CourseTypeID = ? where ID = ?");
+        $stmt->bind_param("sisiiisiiiii", $course->getName(), $course->getPostCode(), $course->getPlace(), $course->getStart(), $course->getCosts(), $course->getEnd(),
+                $course->getLink(), $course->getInstituteId(), $course->getDepartmentId(), $course->getAreaId(), $course->getCourseTypeId(), $course->getId());
+        
+        $stmt->execute();
+        
+        return $this->read($course->getId());
+    }
+    
+    public function delete(Course $course){
+        $stmt = $this->mysqli->prepare("DELETE FROM course WHERE ID = ?");
+        $stmt->bind_param("i", $course->getId());
+        
+        if($stmt->execute()){
+            return true;
+        }
+        
+        return false;
     }
 }
--- database/InstituteDAO.php
+++ database/InstituteDAO.php
@@ -6,11 +6,46 @@
  * and open the template in the editor.
  */
 
+namespace database;
+
+use model\Institute;
+use model\InvoiceAddress;
+
 /**
  * Description of InstituteDAO
  *
  * @author bodog
  */
 class InstituteDAO extends BasicDAO {
-    //put your code here
+
+    public function create(Instute $institute, InvoiceAddress $invoiceAddress) {
+
+        $stmtInvoiceAddress = $this->mysqli->prepare("Insert into invoiceAddress (ID, Street, HouseNumber, PostCode, Place)"
+                . "VALUES (?, ?, ?, ?, ?");
+
+        $stmtInvoiceAddress->bind_param("issis", Null, $invoiceAddress->getStreet(), $invoiceAddress->getHouseNumber(), $invoiceAddress->getPostCode(), $invoiceAddress->getPlace());
+        $stmtInvoiceAddress->execute;
+
+        $stmtInstitute = $this->mysqli->prepare("Insert into institute (ID, Name, Street, HouseNumber, PostCode, Place, Email, Password, InvoiceAddressID)"
+                . "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmtInstitute->bind_param("isssisssi", Null, $institute->getName(), $institute->getStreet(), $institute->getHouseNumber(), $institute->getPostCode(), $institute->getPlace(), $institute->getEmail(), $institute->getPassword(), $invoiceAddress->getId());
+
+        return $this->read($this->mysqli->lastInsertId());
+    }
+
+    public function read($instituteId) {
+        $stmt = $this->mysql->prepare("Select * from institute where ID = ?");
+        $stmt->bind_param("i", $instituteId);
+
+        if ($stmt->execute()) {
+            return $stmt->fetch_all($mysqli::fetch_object)[0];
+        }
+        
+        return null;
+    }
+
+    public function update(Institute $institute) {
+        
+    }
+
 }
