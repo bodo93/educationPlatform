de10affe64d06a0739da989e1a8877d932ca32aa
Hackerman <32144115+Hackermanbitch@users.noreply.github.com>
2018-11-08 12:30:35 +0100

    dbRegistration code in InstituteController now

--- controller/InstituteController.php
+++ controller/InstituteController.php
@@ -17,7 +17,7 @@ class InstituteController {
     
     public static function register($view = null){
         //register institute
-        $institute = new \model\Institute();
+        /*$institute = new \model\Institute();
         //$institute->setId($_POST("id"));
         $institute->setName($_POST["name"]);
         $institute->setStreet($_POST["street"]);
@@ -34,6 +34,60 @@ class InstituteController {
         $invoiceAddress->setPlace($_POST["invPlace"]);
         //set foreignKey
         $institute->setInvoiceAddressId($invoiceAddress->getId());
+         * 
+         */
+        
+        $myschool = $_POST['name'];
+        $myaddress = $_POST['street'];
+        $myHouseNumber = $_POST['houseNumber'];
+        $myPostCode = $_POST['postCode'];
+        $myCity = $_POST['place'];
+        $myEmail = $_POST['email'];
+        $myPassword = $_POST['password'];
+        $myPassword2 = $_POST['password2'];
+        $encrypt = password_hash($myPassword, PASSWORD_DEFAULT, ['cost' => 12]);
+
+        $db = \dbConnection::getConnection();
+        $mysqli = $db->getConnection();
+
+        $select = "SELECT Email FROM institute WHERE Email = '$myEmail'";
+        $result = $mysqli->query($select);
+
+        $count = mysqli_num_rows($result);
+        if($count == 1) {
+            echo "
+                <script type=\"text/javascript\">
+                alert('Username already exists!');
+                window.location.replace('register');
+                </script>
+            ";
+        }else{
+            if($myPassword == $myPassword2){
+                $insert = "INSERT INTO institute (Name, Street, HouseNumber, PostCode, Place, Email, Password)
+                        VALUES ('$myschool', '$myaddress', '$myHouseNumber', '$myPostCode', '$myCity', '$myEmail', '$encrypt')";
+
+                if ($mysqli->query($insert) === TRUE) {
+                    echo "
+                        <script type=\"text/javascript\">
+                        alert('User successfully created');
+                        window.location.replace('login');
+                        </script>
+                    ";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+
+                $conn->close();
+            }
+            else{
+                echo "
+                <script type=\"text/javascript\">
+                alert('Your Passwords do not match!');
+                window.location.replace('register.php');
+                </script>
+                ";
+            }
+        }
     }
     
     public static function login(){
