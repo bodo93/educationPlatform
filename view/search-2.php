3d0f0076c59d4d20a8f1c9b55d7fc687374e6f77
Rene <Rene@10.0.1.34>
2018-11-14 10:13:28 +0100

    Merge origin/master
    Conflicts:
    	index.php

---
+++ view/search-2.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Home - Brand</title>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+        <link rel="stylesheet" href="assets/css/Data-Table.css">
+        <link rel="stylesheet" href="assets/css/dh-row-titile-text-image-right-1.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+        <link rel="stylesheet" href="assets/css/smoothproducts.css">
+        <link rel="stylesheet" href="assets/css/Table-With-Search.css">
+    </head>
+
+    <body style="background-color: rgb(34,36,37);">
+        <header>
+            <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+                <div class="container"><a class="navbar-brand logo" href="search.html" style="margin-right: 0px;"><img src="assets/img/Logo2.png" id="logo" style="width: 180px;height: 65px;"></a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div
+                        class="collapse navbar-collapse" id="navcol-1">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="myCourses.html" style="font-size: 14px;font-weight: bold;">Meine Kurse</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="showAccount.html" style="font-size: 14px;font-weight: bold;">Benutzerprofil</a></li>
+                            <li class="nav-item" role="presentation" style="padding-right: 90px;"><a class="nav-link" href="logout.php" style="font-size: 14px;font-weight: bold;">Abmelden</a></li>
+                            <li class="nav-item" role="presentation" style="padding-right: 0px;"><a class="nav-link" href="xxx" style="font-size: 14px;font-weight: bold;">de</a></li>
+                            <li class="nav-item" role="presentation" style="padding-right: 20px;"><a class="nav-link" href="xxx" style="padding-left: 0px;font-size: 14px;font-weight: bold;">EN</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main class="page landing-page" style="margin-top: 90px;">
+            <section class="portfolio-block hire-me" style="background-image: url(&quot;assets/img/classroom3.jpg&quot;);background-size: cover;background-repeat: no-repeat;background-color: transparent;color: #000000;padding-top: 80px;height: 660px;">
+                <div class="container" style="max-width: 420px;margin-top: 50px;">
+                    <div class="heading">
+                        <h2 style="margin-bottom: 20px;font-weight: bold;color: rgb(255,255,255);">Kurs Suche&nbsp;</h2>
+                    </div>
+                    <form style="margin-right: 200;">
+                        <div class="form-group"><label for="subject" style="font-weight: bold;color: rgb(255,255,255);margin-bottom: 2px;">Fachbereich</label><select class="form-control" id="subject"><option value="0"></option><option value="1">Wirtschaft</option><option value="2">IT / Technik</option><option value="4">Recht</option><option value="5">Psychologie </option><option value="6">Sprachen</option><option value="7">Sonstiges</option></select></div>
+                        <div
+                            class="form-group"><label for="subject" style="font-weight: bold;color: rgb(255,255,255);margin-bottom: 2;">Raum</label><select class="form-control" id="subject"><option value="" selected=""></option><option value="1">Westschweiz</option><option value="2">Mittelland</option><option value="3">Nordwestschweiz / Zürich</option><option value="4">Ostschweiz</option><option value="5">Tessin / Wallis</option></select></div>
+                        <div
+                            class="form-group"><label for="subject" style="font-weight: bold;color: rgb(255,255,255);margin-bottom: 2px;">Kurs Typ</label><select class="form-control" id="subject"><option value="" selected=""></option><option value="1">Bachelor</option><option value="2">Master</option><option value="3">Sonstiges</option></select></div>
+                        <div
+                            class="form-group">
+                            <div class="form-row">
+                                <div class="col-md-6"><label for="hire-date" style="font-weight: bold;color: rgb(255,255,255);margin-bottom: 2px;">Suchbegriff</label><input class="form-control" type="text"></div>
+                                <div class="col-md-6 button"><a class="btn btn-primary btn-block" role="button" href="searchResult.html" style="margin-top: 26px;font-weight: bold;background-color: rgb(43,47,49);">Suchen</a></div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </main>
+        <footer class="page-footer dark">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3" style="min-width: 240px;">
+                        <h5>Adresse</h5>
+                        <p style="color: rgb(251,251,251);">Swissedu AG<br>Wängirain 53<br>8704 Herrliberg<br></p>
+                    </div>
+                    <div class="col-sm-3">
+                        <h5>Kontakt&nbsp;</h5>
+                        <ul>
+                            <li><a href="mailto:name@email.com">info@swissedu.ch</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-3" style="min-width: 240px;">
+                        <h5>Leistungen</h5>
+                        <ul>
+                            <li><a href="ourOffer.html">Angebot</a></li>
+                            <li><a href="terms.html" style="min-width: 160px;">Terms</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-3" style="min-width: 240px;">
+                        <h5>Login</h5>
+                        <ul>
+                            <li><a href="login.html">Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright" style="margin-top: 0px;">
+                <p>© 2018 Copyright SWISSEDU</p>
+            </div>
+        </footer>
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+        <script src="assets/js/smoothproducts.min.js"></script>
+        <script src="assets/js/theme.js"></script>
+        <script src="assets/js/createMyCoursesTable.js"></script>
+        <script src="assets/js/Table-With-Search.js"></script>
+    </body>
+
+</html>
