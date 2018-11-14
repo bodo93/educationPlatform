cb77ff28f594fa671c7144d50398a12f732bdaf4
Hackerman <32144115+Hackermanbitch@users.noreply.github.com>
2018-11-14 12:04:20 +0100

    search works again

--- view/search.php
+++ view/search.php
@@ -14,6 +14,18 @@ author: Philipp Lehmann
         <title><?php echo $lang['title']?></title>
     </head>
     <body style="background-color: rgb(34,36,37);">
+        <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+            <div class="container"><a class="navbar-brand logo" href="search.html" style="margin-right: 0px;"><img src="assets/img/Logo2.png" id="logo" style="width: 180px;height: 65px;"></a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div
+                    class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item" role="presentation" style="padding-right: 90px;"><a class="nav-link" href="login" style="font-size: 14px;font-weight: bold;">Anmelden</a></li>
+                        <li class="nav-item" role="presentation" style="padding-right: 0px;"><a class="nav-link" href="about-us.html" style="font-size: 14px;font-weight: bold;">de</a></li>
+                        <li class="nav-item" role="presentation" style="padding-right: 20px;"><a class="nav-link" href="about-us.html" style="padding-left: 0px;font-size: 14px;font-weight: bold;">EN</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <main class="page landing-page">
             <section class="portfolio-block hire-me" style="background-image: url(&quot;assets/img/classroom3.jpg&quot;);background-size: cover;background-repeat: no-repeat;background-color: transparent;color: #000000;padding-top: 80px;height: 660px;">
                 <div class="container" style="max-width: 420px;margin-top: 140px">
@@ -46,9 +58,9 @@ author: Philipp Lehmann
                             class="form-group"><label for="subject" style="font-weight: bold;color: rgb(255,255,255);margin-bottom: 2px;"><?php echo $lang['courseType']?></label>
                             <select name="coursetype" class="form-control" id="subject">
                                 <option></option>
-                                <option value="1">Bachelor</option>
-                                <option value="2">Master</option>
-                                <option value="3">Sonstiges</option>
+                                <option value="Bachelor">Bachelor</option>
+                                <option value="Master">Master</option>
+                                <option value="Sonstiges">Sonstiges</option>
                             </select>
                         </div>
                         <div
