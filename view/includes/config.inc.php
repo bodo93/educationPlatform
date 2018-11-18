78c69281991cc9d335e1f8b5c37345a5ffafba3f
René Schwab <Ren Schwab@10.0.1.37>
2018-11-18 13:02:15 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- view/includes/config.inc.php
+++ view/includes/config.inc.php
@@ -1,9 +1,7 @@
 <?php
-    $DOMAIN_URL = 'http://localhost/educationPlatform';
+    //$DOMAIN_URL = 'http://localhost/educationPlatform';
 
-/** 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    $DOMAIN_URL = 'https://swissedu.herokuapp.com';
+
+
 ?>
