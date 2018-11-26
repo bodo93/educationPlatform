a1cf6a61c2bd5bb1a683d1c735d3a66729c7b62d
bodog <bodog@DESKTOP-39D644H.edu.ds.fhnw.ch>
2018-11-26 19:40:23 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- view/courseEdit_Test.php
+++ view/courseEdit_Test.php
@@ -65,7 +65,8 @@ use database\DBConnection;
                                     . "WHERE `ID` = '" . $_POST['id'] . "'";
                             $result = $mysqli->query($update);
                             if ($result) {
-                                //header("Location: ".$GLOBALS["ROOT_URL"]."/course/overview");
+                                echo "<h3>ERFOLG!!</h3>"
+                                header("Location: ".$GLOBALS["ROOT_URL"]."/course/overview");
                             } else {
                                 echo "Error: " . $update . "<br>" . mysqli_error($conn);
                             }
