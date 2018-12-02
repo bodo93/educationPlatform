785abacdddec5de0ed3d1f3f9ca003f98d054f71
René Schwab <Ren Schwab@10.0.1.37>
2018-12-02 17:44:02 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- controller/EmailController.php
+++ controller/EmailController.php
@@ -10,54 +10,70 @@ use service\EmailServiceClient;
 
 class EmailController {
     
-        // public static function pwResetMail($mail, $userId, $pwHacheCode) {
-        public static function pwResetMail($mail, $userId) {
-            $to = '$mail';
-            $subject = 'SWISSEDU password reset link';
-            $message = 'hello'; // Text: Clic this link to reset your PW + Link 
-            $headers = 'From: webmaster@example.com' . "\r\n" .
-                'Reply-To: webmaster@example.com' . "\r\n" .
-                'X-Mailer: PHP/' . phpversion();
 
-            mail($to, $subject, $message, $headers); // standard PHP function
-    }
-    
      public static function resetPw() {
         $db = DBConnection::getConnection();
         $mysqli = $db->getConnection();
-
-        $stmt = $mysqli->prepare("SELECT * FROM institute WHERE Email = ?");
         
-        $stmt->bind_param('s', $email);
-        $email = $_POST['email'];
+        $submittedEmail = $_POST['email'];
         echo $email;
         
+        $stmt = $mysqli->prepare("SELECT * FROM institute WHERE Email = ?");
+        $stmt->bind_param('si', $email, $id);
+        $email = $submittedEmail;
         $stmt->execute();
         $institute = $stmt->get_result()->fetch_object("model\Institute");
         $stmt->close();
-
-        $pw = $institute->getPassword();
+        
+        // creates random pw with 8 caracters containing a-z, 0-9
+        $pw = bin2hex(openssl_random_pseudo_bytes(4)); 
         echo $pw;
+        
+        
+        if ($institute) {
+          
+            $subject = "SWISSEDU";
+            //$text = $lang['newPwText'];
+            //echo $text.$pw;
+            //$htmlData = $text . $pw;
+            
+            $htmlData = "New Password: ". $pw;
 
-        $subject = "SWISSEDU";
+            // send new pw to user
+            EmailServiceClient::sendEmail($email, $subject, $htmlData);
 
-        $text = $lang['newPwText'];
-        echo $text;
-        echo $pw;
-                
-        $htmlData = $text. $pw;
-        
+            /*
+            // update new pw (encrypted) to database 
+            $stmt = $mysqli->prepare("UPDATE institute SET `Password` = ? WHERE `ID` = ?");
 
-        EmailServiceClient::sendEmail($email, $subject, $htmlData);
-        
-        header("Location: ".$GLOBALS["ROOT_URL"]."/login");
-        
-        
-        echo "  <script type=\"text/javascript\">
+            $stmt->bind_param('si', $encryption, $id);
+   
+            $encryption = $encryption = password_hash($myPassword, PASSWORD_DEFAULT);
+            $id = ???
+            */
+            
+            
+            header("Location: " . $GLOBALS["ROOT_URL"] . "/login");
+   
+            echo "  <script type=\"text/javascript\">
                 alert('Check your E-Mail');
-                
                 </script>
                 ";
+        } else {
+            echo "
+            <script type=\"text/javascript\">
+            alert('Username does not exist');
+            </script>
+            ";
+        }
+
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
