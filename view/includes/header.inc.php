4b9b5b76e5c591b605c653c50eb26c03855474fd
Rene <Rene@10.0.1.34>
2018-12-04 11:49:26 +0100

    Merge origin/master
    Conflicts:
    	controller/CourseController.php

--- view/includes/header.inc.php
+++ view/includes/header.inc.php
@@ -66,6 +66,11 @@ else
                 </div>
             </nav>
         </header>
+        
+        
+        <!--no script tag, if user deactivatted JavaScript-->
+        <noscript>Sorry, your browser does not support JavaScript.<br>Please activate JavaScript to show the website correctly.</noscript> 
+        <!--assets-->
         <script src="assets/js/jquery.min.js"></script>
         <script src="assets/bootstrap/js/bootstrap.min.js"></script>
         <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
