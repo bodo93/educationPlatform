a892ddf0ccdf1ad16b059e4301c71b23091e76bc
René Schwab <Ren Schwab@10.0.1.37>
2018-12-05 10:01:49 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- view/courseCreate.php
+++ view/courseCreate.php
@@ -153,10 +153,10 @@ include 'includes/translator.inc.php';
                     <script>
                         function myFunction() {
                         var x = document.getElementById("infoTxt");
-                        if (x.style.display === "none") {
-                            x.style.display = "none";
-                        } else {
+                        if (!x.style.display === "none") {
                             x.style.display = "block";
+                        } else {
+                            x.style.display = "none";
                             }
                         }
                     </script>
