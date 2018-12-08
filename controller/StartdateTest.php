7e625a7b8b337d1d25c9e7805510b760b68343de
bodog <bodog@192.168.1.105>
2018-12-08 12:10:46 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- controller/StartdateTest.php
+++ controller/StartdateTest.php
@@ -16,14 +16,21 @@ while ($row = mysqli_fetch_assoc($result)) {
     $start = $row["Start"];
     $startTimestamp = strtotime($start);
     $control = $row["ControlNumber"];
-    
-    if ($control == 0){
-        if($startTimestamp <= time()){
-            echo "Datum vom Kurs ".$name." abgelaufen";
+
+    if ($control == 0) {
+        if ($startTimestamp <= time()) {
+            echo "Datum vom Kurs " . $name . " abgelaufen";
             //Email versenden
             $update = $mysqli->prepare("UPDATE course SET `ControlNumber` = ? WHERE `ID` = ?");
+            if ($update === false) {
+                trigger_error($this->mysqli->error, E_USER_ERROR);
+                return;
+            }
             $update->bind_param('ii', 1, $id);
-            $update->execute();
+            $status = $update->execute();
+            if ($status === false) {
+                trigger_error($stmt->error, E_USER_ERROR);
+            }
         } else {
             echo $name . " OK";
             echo "</br>";
