9e08a42f0a95a9a15c6e4c5e4f893577dde50565
bodog <bodog@192.168.1.105>
2018-12-08 18:49:33 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- controller/StartdateTest.php
+++ controller/StartdateTest.php
@@ -1,16 +1,13 @@
 <?php
 use controller\CourseController;
-
-    CourseController::checkStartDate();
-    echo "OK";
     
         $db = DBConnection::getConnection();
         $mysqli = $db->getConnection();
 
         $select = "SELECT ID, Name, CreationDate FROM course";
         if($result = $mysqli->query($select)){
-            echo "OK2";
-        } else {echo "Not OK2";};
+            echo "OK10";
+        } else {echo "Not OK2";}
         
         while ($row = mysqli_fetch_assoc($result)) {
             $id = $row["ID"];
