a85bce7f00dc47621cf42b8b6c26630e5bae0f89
bodog <bodog@192.168.1.105>
2018-12-08 22:46:54 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- controller/StartdateTest.php
+++ controller/StartdateTest.php
@@ -1,58 +1,6 @@
 <?php
+use controller\CourseController;
 
-namespace controller;
+echo CourseController::getDateOfInvoice(98);
 
-use database\DBConnection;
-
-$db = DBConnection::getConnection();
-$mysqli = $db->getConnection();
-
-/*$select = "SELECT ID, Name, Start, ControlNumber FROM course";
-$result = $mysqli->query($select);
-
-while ($row = mysqli_fetch_assoc($result)) {
-    $id = $row["ID"];
-    $name = $row["Name"];
-    $start = $row["Start"];
-    $startTimestamp = strtotime($start);
-    $control = $row["ControlNumber"];
-
-    if ($control == 0) {
-        if ($startTimestamp <= time()) {
-            echo $name . " abgelaufen";
-            //Email versenden
-            $update = $mysqli->prepare("Update course SET `ControlNumber` = ? WHERE `ID` = ?");
-            $number = 1;
-            $update->bind_param('ii', $number, $id);
-            $update->execute();
-        } else {
-            echo $name . " OK"; 
-           echo "</br>";
-        }
-    } else {
-        echo $name . ": Mail wurde bereits versendet";
-    }
-}*/
-
-$select = "SELECT ID, Name, CreationDate FROM course";
-$result = $mysqli->query($select);
-
-while ($row = mysqli_fetch_assoc($result)) {
-    $id = $row["ID"];
-    $name = $row["Name"];
-    $creationDate = $row["CreationDate"];
-    $creationTimestamp = strtotime($creationDate);
-    $creationDateFormat = date('d.m.Y', $creationTimestamp);
-    $dateOfInvoiceTimestamp = $creationTimestamp + ((60 * 60 * 24)*30);
-    $dateOfInvoiceFormat = date('d.m.Y', $dateOfInvoiceTimestamp);
-    
-    /*if($creationTimestamp <= time()){
-        echo $creationDateFormat;
-        echo $dateOfInvoiceFormat;
-    }
-    
-    if($dateOfInvoice>time()){
-        echo "OK";
-    }*/
-}
 ?>
