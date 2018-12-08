321dd8d5fc83880918d4614392c7421d67f46971
bodog <bodog@192.168.1.105>
2018-12-08 11:59:21 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- controller/StartdateTest.php
+++ controller/StartdateTest.php
@@ -21,17 +21,9 @@ while ($row = mysqli_fetch_assoc($result)) {
         if($startTimestamp <= time()){
             echo "Datum vom Kurs ".$name." abgelaufen";
             //Email versenden
-            /*$update = $mysqli->prepare("UPDATE course SET `ControlNumber` = ? WHERE `ID` = ?");
+            $update = $mysqli->prepare("UPDATE course SET `ControlNumber` = ? WHERE `ID` = ?");
             $update->bind_param('ii', 1, $id);
-            $update->execute();*/
-            
-            //$update = "Update bildungsangebot set `Bezeichnung` = '" . $_POST['bezeichnung'] . "', `Kosten` = '" . $_POST['kosten'] . "', `Max_Teilnehmerzahl` = '" . $_POST['max_teilnehmerzahl'] . "', `Startdatum` = '" . $_POST['startdatum'] . "', `Enddatum` = '" . $_POST['enddatum'] . "', `Ort` = '" . $_POST['ort'] . "', `FK_Bildungsinstitut` = '" . $_POST['bildungsinstitut'] . "', `FK_Fachbereich`= '" . $_POST['fachbereich'] . "', `FK_Abschluss` = '" . $_POST['abschluss'] . "' where `ID` = '" . $_POST['id'] . "'";
-            $update = "Update course SET `ControlNumber` = 1 WHERE `ID` = '" . $id . "'";
-            if(mysqli_query(mysqli, $update)){
-                echo "alles OK";
-            } else {
-                echo "Fehler";
-            }
+            $update->execute();
         }
     } else {
         echo "Mail wurde bereits versendet";
