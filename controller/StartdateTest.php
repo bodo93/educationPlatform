0aed3f474ab8699d2316b6b00516efc871da1cc9
bodog <bodog@192.168.1.105>
2018-12-08 18:59:31 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- controller/StartdateTest.php
+++ controller/StartdateTest.php
@@ -1,14 +1,40 @@
 <?php
 //use controller\CourseController;
     
-echo "OK";
-        $db = DBConnection::getConnection();
+$db = DBConnection::getConnection();
         $mysqli = $db->getConnection();
 
-        $testquery = "SELECT CreationDate FROM course where ID = 98";
-        $result = $mysqli->query($testquery);
-        $row = mysqli_fetch_assoc($result);
-        echo $row["CreationDate"];
-        
-    //CourseController::checkDateOfInvoice();
+        $select = "SELECT ID, Name, Start, ControlNumber FROM course";
+        $result = $mysqli->query($select);
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            $id = $row["ID"];
+            $name = $row["Name"];
+            $start = $row["Start"];
+            $startTimestamp = strtotime($start);
+            $control = $row["ControlNumber"];
+
+
+            if ($control == 0) {
+                if ($startTimestamp <= time()) {
+
+                    $selectMail = "SELECT institute.Email from institute JOIN course on institute.ID = course.InstituteID WHERE course.ID = " . $id;
+
+                    if ($result = $mysqli->query($selectMail)) {
+                        $row = mysqli_fetch_assoc($result);
+                        $mail = $row["Email"];
+                    }
+
+                    $toEmail = "$mail";
+                    $subject = "SWISSEDU Notification";
+                    $htmlData = "Your published course " . $name . " has started. Under my courses the course data can be modified as required.";
+                    EmailServiceClient::sendEmail($toEmail, $subject, $htmlData);
+
+                    $update = $mysqli->prepare("Update course SET `ControlNumber` = ? WHERE `ID` = ?");
+                    $number = 1;
+                    $update->bind_param('ii', $number, $id);
+                    $update->execute();
+                } else {echo $name;}
+            }
+        }
 ?>
