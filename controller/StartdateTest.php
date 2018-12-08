6504783905e3bf6310b7cc17fe89b7e8ff73ccbe
bodog <bodog@192.168.1.105>
2018-12-08 19:02:32 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- controller/StartdateTest.php
+++ controller/StartdateTest.php
@@ -1,40 +1,36 @@
 <?php
-//use controller\CourseController;
-    
+
+namespace controller;
+
+use database\DBConnection;
+
 $db = DBConnection::getConnection();
-        $mysqli = $db->getConnection();
+$mysqli = $db->getConnection();
 
-        $select = "SELECT ID, Name, Start, ControlNumber FROM course";
-        $result = $mysqli->query($select);
+$select = "SELECT ID, Name, Start, ControlNumber FROM course";
+$result = $mysqli->query($select);
 
-        while ($row = mysqli_fetch_assoc($result)) {
-            $id = $row["ID"];
-            $name = $row["Name"];
-            $start = $row["Start"];
-            $startTimestamp = strtotime($start);
-            $control = $row["ControlNumber"];
+while ($row = mysqli_fetch_assoc($result)) {
+    $id = $row["ID"];
+    $name = $row["Name"];
+    $start = $row["Start"];
+    $startTimestamp = strtotime($start);
+    $control = $row["ControlNumber"];
 
-
-            if ($control == 0) {
-                if ($startTimestamp <= time()) {
-
-                    $selectMail = "SELECT institute.Email from institute JOIN course on institute.ID = course.InstituteID WHERE course.ID = " . $id;
-
-                    if ($result = $mysqli->query($selectMail)) {
-                        $row = mysqli_fetch_assoc($result);
-                        $mail = $row["Email"];
-                    }
-
-                    $toEmail = "$mail";
-                    $subject = "SWISSEDU Notification";
-                    $htmlData = "Your published course " . $name . " has started. Under my courses the course data can be modified as required.";
-                    EmailServiceClient::sendEmail($toEmail, $subject, $htmlData);
-
-                    $update = $mysqli->prepare("Update course SET `ControlNumber` = ? WHERE `ID` = ?");
-                    $number = 1;
-                    $update->bind_param('ii', $number, $id);
-                    $update->execute();
-                } else {echo $name;}
-            }
+    if ($control == 0) {
+        if ($startTimestamp <= time()) {
+            echo $name . " abgelaufen";
+            //Email versenden
+            $update = $mysqli->prepare("Update course SET `ControlNumber` = ? WHERE `ID` = ?");
+            $number = 1;
+            $update->bind_param('ii', $number, $id);
+            $update->execute();
+        } else {
+            echo $name . " OK"; 
+           echo "</br>";
         }
+    } else {
+        echo $name . ": Mail wurde bereits versendet";
+    }
+}
 ?>
