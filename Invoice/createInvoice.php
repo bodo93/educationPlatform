7945f8329c7fc25db0ff1a249da6d3ff926163b8
bodog <bodog@192.168.1.105>
2018-12-08 23:37:01 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- Invoice/createInvoice.php
+++ Invoice/createInvoice.php
@@ -1,90 +1,106 @@
 <?php
-    ob_start();
+
+ob_start();
+
 //
 // exemple de facture avec mysqli
 // gere le multi-page
 // Ver 1.0 THONGSOUME Jean-Paul
 //
+//use controller\CourseController;
+use database\DBConnection;
 
-    use controller\CourseController;
-    use database\DBConnection;
-    require('PDF_Invoice.php');
-    
-    $db = DBConnection::getConnection();
-    $mysqli = $db->getConnection();
+require('PDF_Invoice.php');
 
-    $stmt = $mysqli->prepare("SELECT * FROM institute WHERE ID = ?");
+$db = DBConnection::getConnection();
+$mysqli = $db->getConnection();
 
-    $stmt->bind_param('s', $userID);
-    $userID = $_SESSION['userID'];
+$stmt = $mysqli->prepare("SELECT * FROM institute WHERE ID = ?");
+
+$stmt->bind_param('s', $userID);
+$userID = $_SESSION['userID'];
+$stmt->execute();
+$institute = $stmt->get_result()->fetch_object("model\Institute");
+$stmt->close();
+
+if (!empty($_GET['id'])) {
+    $id = $_REQUEST['id'];
+
+    $stmt = $mysqli->prepare("SELECT * FROM course WHERE ID = ?");
+
+    $stmt->bind_param('s', $courseID);
+    $courseID = $id;
     $stmt->execute();
-    $institute = $stmt->get_result()->fetch_object("model\Institute");
+    $course = $stmt->get_result()->fetch_object("model\Course");
     $stmt->close();
-    
-    if (!empty($_GET['id'])) {
-        $id = $_REQUEST['id'];
-        
-        $stmt = $mysqli->prepare("SELECT * FROM course WHERE ID = ?");
+}
 
-        $stmt->bind_param('s', $courseID);
-        $courseID = $id;
-        $stmt->execute();
-        $course = $stmt->get_result()->fetch_object("model\Course");
-        $stmt->close();
-    }
-    
-    //get all infos
-    $priceToPay = 60;
-        
-    $dateToday = date("d.m.Y");
-    $dateToPay = date('d.m.Y', strtotime($dateToday. ' + 30 days'));
-    
-    //$dateToday = CourseController::getDateOfCreation($id);
-    //$dateToPay = CourseController::getDateOfInvoice($id);
-    
-    $companyAddress = ( "Swissedu AG\n" .
-                      "8704 Herrliberg\n" .
-                      "Tel.: 044 439 40 20\n" .
-                      "https://swissedu.herokuapp.com"
-            );
-   
-    $recipientAddress = ( $institute->getName() . "\n" . 
-                      $institute->getStreet() . " " .
-                      $institute->getHouseNumber() . "\n" . 
-                      $institute->getPostCode() . " " . $institute->getPlace()
-            );
-    
-    // le mettre au debut car plante si on declare $mysqli avant !
-    $pdf = new PDF_Invoice( 'P', 'mm', 'A4' );
-    $pdf->AddPage();
-    
-    // add Logo to Invoice
-    $pdf->addLogo('view/assets/img/Logo.png');
-        
-    // add addresses to Invoice
-    $pdf->addCompany( $companyAddress );
-    $pdf->addRecipient( $recipientAddress );
-        
-    $pdf->addDate( 'Herrliberg, ', $dateToday );
-    $pdf->addPP("P.P 8704 Herrliberg");
-    $pdf->addTotal($priceToPay, $dateToday, $dateToPay);
-    $pdf->addTitle();
-    $pdf->addNote( "Besten Dank fuer die Benuetzung unserer Dienstleistung.\n" .
-                    "Bitte pruefen Sie diese Rechnung bevor Sie die Zahlung vornehmen.");
-    $pdf->addDetails($dateToday, $course->getName(), $priceToPay);
-    
-    // add Bill to Invoice 
-    $pdf->Image('Invoice/bill.png',0,195,210,0,'PNG');
-    
-    // add Details to Invoice
-    $pdf->addPriceToBill($priceToPay);
-    $pdf->addCompanyToBill( $companyAddress );
-    $pdf->addRecipientToBill( $recipientAddress );
-    $pdf->addAccountToBill();
-    $pdf->addReferenceToBill();
-    
-    // $file = $pdf->Output("I");
-    $file = $pdf->Output('D', "Invoice_".$dateToday.".pdf");
+//get all infos
+$priceToPay = 60;
 
-    ob_end_flush(); 
+//DATE
+$select = "SELECT ID, Name, CreationDate FROM course where ID =" . $courseID;
+$result = $mysqli->query($select);
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $id = $row["ID"];
+    $name = $row["Name"];
+    $creationDate = $row["CreationDate"];
+    $creationTimestamp = strtotime($creationDate);
+    $creationDateFormat = date('d.m.Y', $creationTimestamp);
+    $dateOfInvoiceTimestamp = $creationTimestamp + ((60 * 60 * 24) * 30);
+    $dateOfInvoiceFormat = date('d.m.Y', $dateOfInvoiceTimestamp);
+}
+
+//$dateToday = date("d.m.Y");
+//$dateToPay = date('d.m.Y', strtotime($dateToday . ' + 30 days'));
+
+$dateToday = $creationDateFormat;
+$dateToPay = $dateOfInvoiceFormat;
+
+$companyAddress = ( "Swissedu AG\n" .
+        "8704 Herrliberg\n" .
+        "Tel.: 044 439 40 20\n" .
+        "https://swissedu.herokuapp.com"
+        );
+
+$recipientAddress = ( $institute->getName() . "\n" .
+        $institute->getStreet() . " " .
+        $institute->getHouseNumber() . "\n" .
+        $institute->getPostCode() . " " . $institute->getPlace()
+        );
+
+// le mettre au debut car plante si on declare $mysqli avant !
+$pdf = new PDF_Invoice('P', 'mm', 'A4');
+$pdf->AddPage();
+
+// add Logo to Invoice
+$pdf->addLogo('view/assets/img/Logo.png');
+
+// add addresses to Invoice
+$pdf->addCompany($companyAddress);
+$pdf->addRecipient($recipientAddress);
+
+$pdf->addDate('Herrliberg, ', $dateToday);
+$pdf->addPP("P.P 8704 Herrliberg");
+$pdf->addTotal($priceToPay, $dateToday, $dateToPay);
+$pdf->addTitle();
+$pdf->addNote("Besten Dank fuer die Benuetzung unserer Dienstleistung.\n" .
+        "Bitte pruefen Sie diese Rechnung bevor Sie die Zahlung vornehmen.");
+$pdf->addDetails($dateToday, $course->getName(), $priceToPay);
+
+// add Bill to Invoice 
+$pdf->Image('Invoice/bill.png', 0, 195, 210, 0, 'PNG');
+
+// add Details to Invoice
+$pdf->addPriceToBill($priceToPay);
+$pdf->addCompanyToBill($companyAddress);
+$pdf->addRecipientToBill($recipientAddress);
+$pdf->addAccountToBill();
+$pdf->addReferenceToBill();
+
+// $file = $pdf->Output("I");
+$file = $pdf->Output('D', "Invoice_" . $dateToday . ".pdf");
+
+ob_end_flush();
 ?>
