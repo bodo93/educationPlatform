9357eb4ff3c2280e17fa9129d75f485b7997d92b
bodog <bodog@192.168.1.105>
2018-12-08 11:44:37 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- controller/StartdateTest.php
+++ controller/StartdateTest.php
@@ -7,10 +7,11 @@ use database\DBConnection;
 $db = DBConnection::getConnection();
 $mysqli = $db->getConnection();
 
-$stmt = "SELECT Name, Start, ControlNumber FROM course";
-$result = $mysqli->query($stmt);
+$select = "SELECT ID, Name, Start, ControlNumber FROM course";
+$result = $mysqli->query($select);
 
 while ($row = mysqli_fetch_assoc($result)) {
+    $id = $row["ID"];
     $name = $row["Name"];
     $start = $row["Start"];
     $startTimestamp = strtotime($start);
@@ -20,7 +21,12 @@ while ($row = mysqli_fetch_assoc($result)) {
         if($startTimestamp <= time()){
             echo "Datum vom Kurs ".$name." abgelaufen";
             //Email versenden
+            $update = $mysqli->prepare("UPDATE course SET `ControlNumber` = ? WHERE `ID` = ?");
+            $update->bind_param('ii', 1, $id);
+            $update->execute();
         }
+    } else {
+        echo "Mail wurde bereits versendet";
     }
 }
 ?>
