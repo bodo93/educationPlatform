56b07effe0281b736980ef113febaa0d3bacdc43
bodog <bodog@192.168.1.105>
2018-12-09 13:25:59 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- controller/StartdateTest.php
+++ controller/StartdateTest.php
@@ -1,20 +1,23 @@
 <?php
+
 use database\DBConnection;
 
-        $db = DBConnection::getConnection();
-        $mysqli = $db->getConnection();
+$db = DBConnection::getConnection();
+$mysqli = $db->getConnection();
 
-        $select = "SELECT ID, Name, CreationDate FROM course where ID =" . $id;
-        $result = $mysqli->query($select);
+//DATE
+$selectCreationDate = "SELECT ID, Name, CreationDate FROM course where ID =" . $courseID;
+$result = $mysqli->query($selectCreationDate);
 
-        while ($row = mysqli_fetch_assoc($result)) {
-            $id = $row["ID"];
-            $name = $row["Name"];
-            $creationDate = $row["CreationDate"];
-            $dateOfInvoiceTimestamp = strtotime($creationDate);
-            $dateOfInvoiceFormat = date('d.m.Y', $dateOfInvoiceTimestamp);
-            
-            echo $dateOfInvoiceFormat;
-        }
-
+while ($row = mysqli_fetch_assoc($result)) {
+    $id = $row["ID"];
+    $name = $row["Name"];
+    $creationDate = $row["CreationDate"];
+    $dateOfInvoiceTimestamp = strtotime($creationDate);
+    $dateOfInvoiceFormat = date('d.m.Y', $dateOfInvoiceTimestamp);
+    $dueDateTimestamp = $dateOfInvoiceTimestamp + ((60 * 60 * 24) * 30);
+    $dueDateFormat = date('d.m.Y', $dueDateTimestamp);
+    
+    echo $dueDateFormat;
+}
 ?>
