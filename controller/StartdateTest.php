3807df16176a205f6f69e9cb82be8dc342e8f998
bodog <bodog@192.168.1.105>
2018-12-09 13:10:50 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- controller/StartdateTest.php
+++ controller/StartdateTest.php
@@ -1,8 +1,49 @@
 <?php
-use controller\CourseController;
+$db = DBConnection::getConnection();
+        $mysqli = $db->getConnection();
 
+        $select = "SELECT ID, Name, CreationDate FROM course where ID =" . $id;
+        $result = $mysqli->query($select);
 
-echo CourseController::getDateOfInvoice(98);
-echo CourseController::getDateOfCreation(98);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $id = $row["ID"];
+            $name = $row["Name"];
+            $creationDate = $row["CreationDate"];
+            $creationTimestamp = strtotime($creationDate);
+            $creationDateFormat = date('d.m.Y', $creationTimestamp);
+            $dateOfReminderTimestamp = $creationTimestamp + ((60 * 60 * 24) * 83);
+            $dateOfReminderFormat = date('d.m.Y', $dateOfReminderTimestamp);
+            $dateOfDeletionTimestamp = $creationTimestamp + ((60 * 60 * 24) * 90);
+            $dateOfDeletionFormat = date('d.m.Y', $dateOfDeletionTimestamp);
+
+            $selectMail = "SELECT institute.Email from institute JOIN course on institute.ID = course.InstituteID WHERE course.ID = " . $id;
+
+            if ($result = $mysqli->query($selectMail)) {
+                $row = mysqli_fetch_assoc($result);
+                $mail = $row["Email"];
+            }
+
+            $toEmail = "$mail";
+            $subject = "SWISSEDU Notification";
+            $htmlData = "Your published course " . $name . " has started. Under my courses the course data can be modified as required.";
+            EmailServiceClient::sendEmail($toEmail, $subject, $htmlData);
+
+            if ($dateOfReminderTimestamp <= time()) {
+                $toEmail = "$mail";
+                $subject = "SWISSEDU Notification";
+                $htmlData = "Your published course " . $name . " will be deleted from SWISSEDU on " . $dateOfDeletionFormat;
+                EmailServiceClient::sendEmail($toEmail, $subject, $htmlData);
+            } elseif ($dateOfDeletionTimestamp <= time()) {
+                $toEmail = "$mail";
+                $subject = "SWISSEDU Notification";
+                $htmlData = "Your published course " . $name . " has been deleted from SWISSEDU.";
+                EmailServiceClient::sendEmail($toEmail, $subject, $htmlData);
+
+                // delete data
+                $stmt = $mysqli->prepare("DELETE FROM course WHERE ID = ?");
+                $stmt->bind_param('i', $id);
+                $stmt->execute();
+            }
+        }
 
 ?>
