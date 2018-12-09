7794c8f0ce446de49131a9e898afdfc4026dc127
bodog <bodog@192.168.1.105>
2018-12-09 13:43:48 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- controller/StartdateTest.php
+++ controller/StartdateTest.php
@@ -2,26 +2,42 @@
 
 use database\DBConnection;
 
-$db = DBConnection::getConnection();
-$mysqli = $db->getConnection();
+        $db = DBConnection::getConnection();
+        $mysqli = $db->getConnection();
 
-//DATE
-$selectCreationDate = "SELECT ID, Name, CreationDate FROM course where ID =" . $courseID;
-$result = $mysqli->query($selectCreationDate);
-$row = mysqli_fetch_assoc($result);
-echo $row["ID"];
+        $select = "SELECT ID, Name, Start, ControlNumber FROM course";
+        $result = $mysqli->query($select);
 
-while ($row = mysqli_fetch_assoc($result)) {
-    $id = $row["ID"];
-    echo $id;
-    $name = $row["Name"];
-    $creationDate = $row["CreationDate"];
-    echo $creationDate;
-    $dateOfInvoiceTimestamp = strtotime($creationDate);
-    $dateOfInvoiceFormat = date('d.m.Y', $dateOfInvoiceTimestamp);
-    $dueDateTimestamp = $dateOfInvoiceTimestamp + ((60 * 60 * 24) * 30);
-    $dueDateFormat = date('d.m.Y', $dueDateTimestamp);
-    
-    echo $dueDateFormat;
-}
+        while ($row = mysqli_fetch_assoc($result)) {
+            $id = $row["ID"];
+            $name = $row["Name"];
+            $start = $row["Start"];
+            $startTimestamp = strtotime($start);
+            $control = $row["ControlNumber"];
+
+
+            if ($control == 0) {
+                if ($startTimestamp <= time()) {
+
+                    $selectMail = "SELECT institute.Email from institute JOIN course on institute.ID = course.InstituteID WHERE course.ID = " . $id;
+
+                    if ($result = $mysqli->query($selectMail)) {
+                        $row = mysqli_fetch_assoc($result);
+                        $mail = $row["Email"];
+                    }
+
+                    $toEmail = "$mail";
+                    $subject = "SWISSEDU Notification";
+                    $htmlData = "Your published course " . $name . " has started. Under my courses the course data can be modified as required.";
+                    EmailServiceClient::sendEmail($toEmail, $subject, $htmlData);
+
+                    $update = $mysqli->prepare("Update course SET `ControlNumber` = ? WHERE `ID` = ?");
+                    $number = 1;
+                    $update->bind_param('ii', $number, $id);
+                    $update->execute();
+                }
+            } else {
+                echo "OK";
+            }
+        }
 ?>
